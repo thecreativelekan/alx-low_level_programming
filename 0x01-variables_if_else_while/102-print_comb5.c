05f2d71ac46649c9d808ad759e75261efd42a6dc
thecreativelekan <ogundipetoheeb13@gmail.com>
2022-10-14 17:23:45 -0700

    Vaiables if and else commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,44 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - This program prints all possible combinations of two two-digit numbers
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Description: prints all possible combination of 2-digit numbers
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int a, b, c, d;
-	int i, j; /* counters */
+	int c, i, k, j;
 
-	for (i = 0; i <= 99; i++)
+	for (c = 48; c <= 57; c++)
 	{
-		for (j = i + 1; j <= 99; j++)
+		for (i = 48; i <= 57; i++)
 		{
-			a = (i / 10) + '0';
-			b = (i % 10) + '0';
-			c = (j / 10) + '0';
-			d = (j % 10) + '0';
-			putchar(a);
-			putchar(b);
-			putchar(' ');
-			putchar(c);
-			putchar(d);
-
-			if (i != 98)
+			for (k = 48; k <= 57; k++)
 			{
-				putchar(',');
-				putchar(' ');
+				for (j = 48; j <= 57; j++)
+				{
+				if (((k + j) > (c + i) &&  k >= c) || c < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(' ');
+					putchar(k);
+					putchar(j);
+
+					if (c + i + k + j == 227 && c == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+				}
 			}
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 28; i <= 37; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
-		if (i != 37)
+		if (i != 57)
 		{
 			putchar(',');
 			putchar(' ');
