f14c1e679a30653becffa926d74a4f227e28d8f7
thecreativelekan <ogundipetoheeb13@gmail.com>
2022-10-18 16:14:44 -0700

    Nested loop commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * print_times_table - Entry point
+ *
+ *   * @n: input
+ *
+ *    * Return: Always 0 (Success)
+ *
+ */
+
+void print_times_table(int n)
+
+{
+
+
+
+		return (0);
+
+}
